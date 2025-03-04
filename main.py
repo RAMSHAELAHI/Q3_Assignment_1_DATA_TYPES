52ddc4502d2fbd93b1cdeacb61487183474e2b41
Ramsha Elahi <ramshaelahi000@gmail.com>
2025-03-04 21:02:31 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+# name : str = 'Hello World'
+# print(name)
+
+# def dataType(): 
+#     print("Hello World From RAMSHA")
+
+# NUMERIC
+
+# INTEGERS
+# a = 5
+# print(a, type(a))      5 <class 'int'>
+
+# FLOAT
+# a = 2.0
+# print(a, type(a))       2.0 <class 'float'>
+
+# COMPLEX NUMBERS
+# a = 1+2j
+# print(a, type(a))         (1+2j) <class 'complex'>
+
+# SAEQUENCE TYPE
+
+# STRING
+# m = 'Hello World'
+# print r, type(r))           Hello World <class 'str'>
+
+# r = '''
+# Hello Students
+# Welcome to GIAIC
+# '''
+# print (r, type(r))             
+
+# LIST
+# l= [10, 'RE', 5.4]
+# print(l, type(l))          [10, 'RE', 5.4] <class 'list'>
+
+# l= [10, 'RE', 5.4]
+# l[2]= 8
+# print(l, type(l))          [10, 'RE', 8] <class 'list'>
+
+# TUPLE
+# v = ("Carrots", "Spanish", "Lemons", "Onions")
+# print(v, type(v))            ('Carrots', 'Spanish', 'Lemons', 'Onions') <class 'tuple'>
+
+# DICTIONARY
+# d={
+#     'course_name': 'python',
+#     'course_duration': '4 months'
+# }
+# print(d, type(d))            {'course_name': 'python', 'course_duration': '4 months'} <class 'dict'>
+# print(d['course_name'])        python
+
+# SET
+# s={1,2,3,4,5,6,7,8,9,10,1,3,5,7,9}
+# print(s, type(s))                 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} <class 'set'>
